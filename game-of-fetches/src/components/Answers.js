6c1d630053db7d90bb204f21ccecee6f0ec6dc43
Teresa Malave <dev.malave7@gmail.com>
2020-04-03 19:42:35 -0500

    deleted constructor and props; worked in displaying in console!

--- game-of-fetches/src/components/Answers.js
+++ game-of-fetches/src/components/Answers.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class Answers extends Component {
-    constructor(props)
-    super(props)
 
    state = {
        data: []
